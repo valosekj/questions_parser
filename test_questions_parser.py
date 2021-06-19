368ccef38dbe9b7142f2913b9973f17c109efda7
valosekj <jan.valosek@upol.cz>
2021-06-19 15:38:15 +0200

    move function description from comments to docstrings

--- test_questions_parser.py
+++ test_questions_parser.py
@@ -8,24 +8,27 @@
 
 from questions_parser import QuestionsParser
 
-# -------------------------------------------------------------------
-# test of removing of multiple spaces
-# -------------------------------------------------------------------
+
 def test_remove_multiple_spaces():
+    """
+    test of removing of multiple spaces
+    """
     questions_parser = QuestionsParser()
     assert questions_parser.remove_multiple_spaces('Sentence  with  multiple     spaces') == 'Sentence with multiple spaces'
 
-# -------------------------------------------------------------------
-# test of removing single spaces (this step is better for comparison)
-# -------------------------------------------------------------------
+
 def test_clear_sentence():
+    """
+    test of removing single spaces (this step is better for comparison)
+    """
     questions_parser = QuestionsParser()
     assert questions_parser.remove_single_space('this is a test answer') == 'thisisatestanswer'
 
-# -------------------------------------------------------------------
-# test of removing lowercase letters at the beginning of answers
-# -------------------------------------------------------------------
+
 def test_remove_lowercase_letters():
+    """
+    test of removing lowercase letters at the beginning of answers
+    """
     questions_parser = QuestionsParser()
     assert questions_parser.remove_lowercase_letters('a. this is a test answer') == 'this is a test answer'
     assert questions_parser.remove_lowercase_letters('b) this is a test answer') == 'this is a test answer'
